2cf62da376070ade92de7645bffde27f1b9a8e92
Matt1s <mattpakan@gmail.com>
2017-04-18 00:32:58 +0200

    17. 4. 2017 A

---
+++ app/src/main/java/com/example/riaditelka/myapplication/Fyzika_Vrhy_VrhSikmy_Infodvgsin2alfa.java
@@ -0,0 +1,13 @@
+package com.example.riaditelka.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fyzika_Vrhy_VrhSikmy_Infodvgsin2alfa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fyzika__vrhy__vrh_sikmy__infodvgsin2alfa);
+    }
+}
