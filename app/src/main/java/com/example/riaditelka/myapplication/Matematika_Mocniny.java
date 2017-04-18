6455db92b6d7e8ca1b66b04267544f9aa2720095
Matt1s <mattpakan@gmail.com>
2017-04-18 19:43:52 +0200

    18. 4. 2017 A

--- app/src/main/java/com/example/riaditelka/myapplication/Matematika_Mocniny.java
+++ app/src/main/java/com/example/riaditelka/myapplication/Matematika_Mocniny.java
@@ -1,7 +1,11 @@
 package com.example.riaditelka.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Matematika_Mocniny extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class Matematika_Mocniny extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matematika__mocniny);
+
+        ImageView img_back = (ImageView) findViewById(R.id.img_back);
+        final Context context =this;
+        img_back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(context, Matematika.class);
+                startActivity(intent);
+            }
+
+
+        });
     }
 }
